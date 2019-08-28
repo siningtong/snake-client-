ba2a2c01825bf6300b653d47d51632242787fb52
siningtong <siningtong@gmail.com>
2019-08-28 23:00:05 +0000

    control c

--- play.js
+++ play.js
@@ -1,3 +1,18 @@
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data',handleUserInput)
+  return stdin;
+}
+function handleUserInput(key){
+  if (key === '\u0003') {
+    process.exit()
+  }
+}
+
+setupInput()
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
