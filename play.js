be6bb75a0247c41178a0f7966f3293f3ac9eb7d5
siningtong <siningtong@gmail.com>
2019-08-28 21:16:27 +0000

    snake client

---
+++ play.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '192.168.88.151',
+    port: 50541
+  });//=> returns a connection object
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+  conn.on('data',(data)=>{
+    console.log('Server says: ', data);
+  })
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
