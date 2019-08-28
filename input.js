705e95e1bd2a282bb65ec17153c436262f7c0016
siningtong <siningtong@gmail.com>
2019-08-28 23:51:28 +0000

    canPlayNow

--- input.js
+++ input.js
@@ -1,4 +1,6 @@
-const setupInput = function() {
+let connection;
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -9,6 +11,18 @@ const setupInput = function() {
 function handleUserInput(key){
   if (key === '\u0003') {
     process.exit()
+  }
+  if(key === 'w'){
+     connection.write('Move: up')
+  }
+  if (key === 's'){
+    connection.write('Move: down')
+  }
+  if (key === 'a'){
+    connection.write('Move: left')
+  }
+  if(key === 'd'){
+    connection.write('Move: right')
   }
 }
 
