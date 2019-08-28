7093462eb26f91cb2bbc419cc4d721aa4b3caa1e
siningtong <siningtong@gmail.com>
2019-08-28 22:22:26 +0000

    move up

--- client.js
+++ client.js
@@ -14,7 +14,14 @@ const connect = function() {
   conn.on('data',(data)=>{
     console.log('Server says: ', data);
   })
-
+  // conn.on('connect', () => {
+  //   console.log('Im moving up')
+  //   conn.write("Move: up");
+  //   setInterval(()=>{
+  //     conn.write("Move: up");
+  //   },50)
+    
+  // })
   return conn;
 }
 module.exports={
